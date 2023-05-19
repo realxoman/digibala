cd6880cfc29589929c1d6fbd2679df4451d48463
armin <genius2001armin@gmail.com>
2023-05-19 13:34:47 +0330

    version 1.0.0

---
+++ server/gallery.go
@@ -0,0 +1,52 @@
+package server
+
+import (
+	"digibala/models"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+func galleryRoutes(e *echo.Echo) {
+	e.GET("/gallery", listGalleryHandler)
+	e.POST("/gallery", createGalleryHandler)
+	e.GET("/gallery/:id", findGalleryHandler)
+	e.DELETE("/gallery/:id", deleteGalleryHandler)
+	e.PUT("/gallery", updateGalleryHandler)
+}
+
+func updateGalleryHandler(c echo.Context) error {
+	gallery := &models.Gallery{}
+	if err := c.Bind(gallery); err != nil {
+		return err
+	}
+	fmt.Println("Updating address id:", gallery.ID)
+	return c.JSON(http.StatusOK, gallery)
+}
+
+func deleteGalleryHandler(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	fmt.Println("Deleting address id:", id)
+	return c.JSON(http.StatusOK, models.StatusOK{OK: "OK"})
+}
+
+func createGalleryHandler(c echo.Context) error {
+	gallery := &models.Gallery{}
+	if err := c.Bind(gallery); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, gallery)
+}
+
+func listGalleryHandler(c echo.Context) error {
+	gallery := &models.Gallery{}
+	return c.JSON(http.StatusOK, gallery)
+}
+
+func findGalleryHandler(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	gallery := &models.Address{ID: id}
+	return c.JSON(http.StatusOK, gallery)
+}
