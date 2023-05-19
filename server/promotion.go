21be0812e0351f4aad4888eff32602bf8b6fe961
Eqbal Amininejad <55484061+eqba1@users.noreply.github.com>
2023-05-19 14:57:07 +0330

    Update promotion.go server

--- server/promotion.go
+++ server/promotion.go
@@ -24,16 +24,7 @@ func promotionHandler(c echo.Context) error {
 func findPromotionHandler(c echo.Context) error {
 	promotionID := c.Param("id")
 	prid, _ := strconv.Atoi(promotionID)
-
-	category := new(models.Category)
-	promotion := models.Promotion{
-		ID:          prid,
-		Name:        "Promotion 1",
-		Category:    *category,
-		Description: "Promotion 1 description",
-		Discount:    0.2,
-		ExpireDate:  time.Now(),
-	}
+	promotion := new(&models.Promotion{})
 
 	return c.JSON(http.StatusOK, promotion)
 }
@@ -49,21 +40,8 @@ func deletePromotionHandler(c echo.Context) error {
 
 func createPromotionHandler(c echo.Context) error {
 	categoryName := c.Param("name")
-
-	category := &models.Category{
-		ID:   1,
-		Name: categoryName,
-	}
-
-	promotion := models.Promotion{
-		ID:          1,
-		Name:        "Promotion 1",
-		Category:    *category,
-		Description: "Promotion 1 description",
-		Discount:    0.2,
-		ExpireDate:  time.Now(),
-	}
-
+	promotion := new(&model.Promotion{})
+	
 	if err := c.Bind(promotion); err != nil {
 		return err
 	}
