325d86b85e45d6726460d95cb41f29af9eab2988
ehsan <ehsanfayez21@gmail.com>
2023-05-19 14:47:31 +0330

    delete sample array for tes crud

--- server/product.go
+++ server/product.go
@@ -2,13 +2,12 @@ package server
 
 import (
 	"digibala/models"
+	"fmt"
 	"net/http"
 	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
-
-var products []models.Product
 
 func productRoutes(e *echo.Echo) {
 	e.GET("/product", indexProductHandler)
@@ -20,17 +19,13 @@ func productRoutes(e *echo.Echo) {
 
 // user routes comes here
 func indexProductHandler(c echo.Context) error {
+	var products []models.Product
 	return c.JSON(http.StatusOK, products)
 }
 func FindProductHandler(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
 
-	for _, product := range products {
-		if product.ID == id {
-			return c.JSON(http.StatusOK, product)
-		}
-	}
-	return c.JSON(http.StatusNotFound, "product not found")
+	return c.JSON(http.StatusNotFound, id)
 }
 func createProductHandler(c echo.Context) error {
 	product := &models.Product{}
@@ -38,46 +33,20 @@ func createProductHandler(c echo.Context) error {
 	if err := c.Bind(product); err != nil {
 		return c.JSON(http.StatusBadRequest, err.Error())
 	}
-
-	products = append(products, *product)
 	return c.JSON(http.StatusCreated, product)
 }
 func updateProductHandler(c echo.Context) error {
 	product := &models.Product{}
 	id, _ := strconv.Atoi(c.Param("id"))
-	var index int
-	find := false
-	for i, product := range products {
-		if product.ID == id {
-			index = i
-			find = true
-		}
-	}
-	if !find {
-		return c.JSON(http.StatusNotFound, "product not found")
-	}
+
 	if err := c.Bind(product); err != nil {
 		return c.JSON(http.StatusBadRequest, err.Error())
 	}
 
-	products[index] = *product
-
-	return c.JSON(http.StatusOK, products[index])
+	return c.JSON(http.StatusOK, id)
 }
 func deleteProductHandler(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
-	var index int
-	find := false
-	for i, product := range products {
-		if product.ID == id {
-			index = i
-			find = true
-		}
-	}
-	if !find {
-		return c.JSON(http.StatusNotFound, "product not found")
-	}
-	products = append(products[:index], products[index+1:]...)
-
+	fmt.Println(id)
 	return c.JSON(http.StatusOK, "product deleted successfully")
 }
