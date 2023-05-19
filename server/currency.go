11c7a3b709a4c4451fb2894369af4fca5a004af3
ehsan <ehsan-rafee@hotmail.com>
2023-05-19 14:07:37 +0330

    fix : currency remove logic from currency api

--- server/currency.go
+++ server/currency.go
@@ -8,35 +8,30 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-var currencies []models.Currency
-
 func listCurrencyHandler(c echo.Context) error {
+	var currencies []models.Currency
 	return c.JSON(http.StatusOK, currencies)
 }
 
 func getCurrencyByIDHandler(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
+	currency := models.Currency{
+		ID: id,
+	}
+
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, "Invalid currency ID")
 	}
 
-	for _, currency := range currencies {
-		if currency.ID == id {
-			return c.JSON(http.StatusOK, currency)
-		}
-	}
-
-	return echo.NewHTTPError(http.StatusNotFound, "Currency not found")
+	return c.JSON(http.StatusOK, currency)
 }
 
 func createCurrencyHandler(c echo.Context) error {
 	currency := new(models.Currency)
+
 	if err := c.Bind(currency); err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, "Invalid currency data")
 	}
-
-	currency.ID = len(currencies) + 1
-	currencies = append(currencies, *currency)
 
 	return c.JSON(http.StatusCreated, currency)
 }
@@ -52,31 +47,18 @@ func updateCurrencyHandler(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, "Invalid currency data")
 	}
 
-	for i := range currencies {
-		if currencies[i].ID == id {
-			currency.ID = id
-			currencies[i] = *currency
-			return c.JSON(http.StatusOK, currency)
-		}
-	}
+	currency.ID = id
 
-	return echo.NewHTTPError(http.StatusNotFound, "Currency not found")
+	return c.JSON(http.StatusOK, currency)
 }
 
 func deleteCurrencyHandler(c echo.Context) error {
-	id, err := strconv.Atoi(c.Param("id"))
+	_, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, "Invalid currency ID")
 	}
 
-	for i := range currencies {
-		if currencies[i].ID == id {
-			currencies = append(currencies[:i], currencies[i+1:]...)
-			return c.NoContent(http.StatusNoContent)
-		}
-	}
-
-	return echo.NewHTTPError(http.StatusNotFound, "Currency not found")
+	return c.NoContent(http.StatusNoContent)
 }
 
 func currencyRoutes(e *echo.Echo) {
