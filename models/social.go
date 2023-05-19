3cd9dd19dccce1dfe4c90b0480b55f5cd537a427
Hassan Mohammadian <hasssanitman@gmail.com>
2023-05-19 13:32:35 +0330

    fix: SocialID to SocialURL

--- models/social.go
+++ models/social.go
@@ -1,8 +1,8 @@
 package models
 
 type Social struct {
-	ID       int
-	Name     string
-	SocialID string
-	Logo     string
+	ID        int
+	Name      string
+	SocialURL string
+	Logo      string
 }
