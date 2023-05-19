e835c191fd51db2c245d045d4e1efd450fbc5bc9
Hassan Mohammadian <hasssanitman@gmail.com>
2023-05-19 13:23:03 +0330

    feat: add social

---
+++ server/social.go
@@ -0,0 +1,48 @@
+package server
+
+import (
+	"digibala/models"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+func socialRoutes(e *echo.Echo) {
+	e.GET("/social", listSocialHandler)
+	e.POST("/social", createSocialHandler)
+	e.GET("/social", getSocialHandler)
+	e.DELETE("/social", deleteSocialHandler)
+	e.PUT("/social", updateSocialHandler)
+}
+
+func listSocialHandler(c echo.Context) error {
+	socials := []models.Social{}
+	return c.JSON(http.StatusOK, socials)
+}
+func createSocialHandler(c echo.Context) error {
+	social := &models.Social{}
+	if err := c.Bind(social); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusCreated, social)
+}
+func getSocialHandler(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	social := &models.Social{ID: id}
+	return c.JSON(http.StatusOK, social)
+}
+func deleteSocialHandler(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	fmt.Println("Deleting social id: ", id)
+	return c.JSON(http.StatusOK, models.StatusOK{OK: "OK"})
+}
+func updateSocialHandler(c echo.Context) error {
+	social := &models.Social{}
+	if err := c.Bind(social); err != nil {
+		return err
+	}
+	fmt.Println("Updatig social id: ", social.ID)
+	return c.JSON(http.StatusOK, social)
+}
