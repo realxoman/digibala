1ebf464dd5dee139a86d053b5d2f3aaf3cf38759
Mostafa Solati <mostafa.solati@gmail.com>
2023-06-01 09:19:52 +0330

    fix minor errors

---
+++ models/teacher.go
@@ -0,0 +1,7 @@
+package models
+
+type Teacher struct {
+	FirstName string
+	LastName  string
+	Email     string
+}
--- server/promotion.go
+++ server/promotion.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"strconv"
-	"time"
 
 	"net/http"
 
@@ -22,9 +21,7 @@ func promotionHandler(c echo.Context) error {
 }
 
 func findPromotionHandler(c echo.Context) error {
-	promotionID := c.Param("id")
-	prid, _ := strconv.Atoi(promotionID)
-	promotion := new(&models.Promotion{})
+	promotion := new(models.Promotion)
 
 	return c.JSON(http.StatusOK, promotion)
 }
@@ -39,9 +36,8 @@ func deletePromotionHandler(c echo.Context) error {
 }
 
 func createPromotionHandler(c echo.Context) error {
-	categoryName := c.Param("name")
-	promotion := new(&model.Promotion{})
-	
+	promotion := new(models.Promotion)
+
 	if err := c.Bind(promotion); err != nil {
 		return err
 	}
@@ -59,7 +55,6 @@ func updatePromotionHandler(c echo.Context) error {
 }
 
 func promotionRoutes(e *echo.Echo) {
-	fmt.Println(models.Promotion{})
 	e.GET("/promotion", promotionHandler)
 	e.GET("/promotion/:id", findPromotionHandler)
 	e.DELETE("/promotion/:id", deletePromotionHandler)
